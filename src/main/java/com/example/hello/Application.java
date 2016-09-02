e503e99a0ec1446c66ecdd4a9a285fce794cb313
biezhi <biezhi.me@gmail.com>
2016-09-02 11:17:44 +0800

    -

--- src/main/java/com/example/hello/Application.java
+++ src/main/java/com/example/hello/Application.java
@@ -19,9 +19,9 @@ import com.blade.web.http.Response;
 public class Application {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		// jdk6
 		$().get("/", new RouteHandler() {
 			public void handle(Request request, Response response) {
@@ -29,13 +29,15 @@ public class Application {
 			}
 		}).get("/users", new RouteHandler() {
 			public void handle(Request request, Response response) {
-				List<User> users = new ArrayList<>(2);
+				List<User> users = new ArrayList<User>(2);
 				users.add(new User("jack", 22));
 				users.add(new User("王爵nice", 24));
 				response.json(users);
 			}
-		}).before("/.*", (req, res) -> {
-			LOGGER.info("before ...");
+		}).before("/.*", new RouteHandler() {
+			public void handle(Request request, Response response) {
+				LOGGER.info("before ...");
+			}
 		}).start(EmbedJettyServer.class);
 
 	}
