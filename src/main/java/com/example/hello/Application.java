7c446aa93b9b4daaf6fda93ac6b182c1f5dcaadc
biezhi <biezhi.me@gmail.com>
2017-02-20 23:49:34 +0800

    => update blade version

--- src/main/java/com/example/hello/Application.java
+++ src/main/java/com/example/hello/Application.java
@@ -1,11 +1,7 @@
 package com.example.hello;
 
 import com.blade.embedd.EmbedJettyServer;
-import com.blade.mvc.http.Request;
-import com.blade.mvc.http.Response;
-import com.blade.mvc.route.RouteHandler;
 import com.blade.mvc.view.ViewSettings;
-import com.blade.mvc.view.template.VelocityTemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,34 +15,22 @@ import static com.blade.Blade.$;
  */
 public class Application {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		ViewSettings.$().templateEngine(new VelocityTemplateEngine());
-		
-		$().get("/", new RouteHandler() {
-			public void handle(Request request, Response response) {
-				response.html("<h1>Hello Blade！</h1>");
-			}
-		}).get("/hello", new RouteHandler() {
-			public void handle(Request request, Response response) {
-				request.attribute("name", "jack");
-				response.render("hello.vm");
-			}
-		}).get("/users", new RouteHandler() {
-			public void handle(Request request, Response response) {
-				List<User> users = new ArrayList<User>(2);
-				users.add(new User("jack", 22));
-				users.add(new User("王爵nice", 24));
-				response.json(users);
-			}
-		}).before("/.*", new RouteHandler() {
-			public void handle(Request request, Response response) {
-				LOGGER.info("before ...");
-			}
-		}).start(EmbedJettyServer.class);
+        ViewSettings.$().templateEngine(new VelocityTemplateEngine());
 
-	}
+        $().get("/", (request, response) -> response.html("<h1>Hello Blade！</h1>")).get("/hello", (request, response) -> {
+            request.attribute("name", "jack");
+            response.render("hello.vm");
+        }).get("/users", (request, response) -> {
+            List<User> users = new ArrayList<>(2);
+            users.add(new User("jack", 22));
+            users.add(new User("王爵nice", 24));
+            response.json(users);
+        }).before("/.*", (request, response) -> LOGGER.info("before ...")).start(EmbedJettyServer.class);
+
+    }
 
 }
